6b6d1c062e6c14562cf9835f5d0926c3fbd97898
Milot Mirdita <milot@mirdita.de>
2023-06-26 15:37:03 +0900

    fix bash-ism in run_search

--- regression/run_search.sh
+++ regression/run_search.sh
@@ -12,14 +12,15 @@ SPLITS=1
 # "${PETASEARCH}" createdb "${TARGET}" "${TARGETDB}"
 # "${PETASEARCH}" splitdb "${TARGETDB}" "$RESULTS/target_sra_split" --split "${SPLITS}"
 
-
 : > "$RESULTS/targetdbs"
 : > "$RESULTS/resultlist"
-for ((i=0; i<${SPLITS}; i++)); do
+i=0
+while [ "$i" -lt "$SPLITS" ]; do
     "${PETASEARCH}" convert2sradb "$RESULTS/target_sra_split_${i}_${SPLITS}" "$RESULTS/target_sra_${i}_${SPLITS}"
     "${PETASEARCH}" createkmertable "$RESULTS/target_sra_${i}_${SPLITS}" "$RESULTS/target_kmer_${i}_${SPLITS}" -k 6 --spaced-kmer-mode 0 --seed-sub-mat PAM30.out
     printf "%s\t%s\n" "$RESULTS/target_kmer_${i}_${SPLITS}" "$RESULTS/target_sra_${i}_${SPLITS}" >> "$RESULTS/targetdbs"
     printf "%s\n" "$RESULTS/res_${i}_${SPLITS}" >> "$RESULTS/resultlist"
+    i=$((i+1))
 done
 
 "${PETASEARCH}" petasearch "${QUERYDB}" "$RESULTS/targetdbs" "$RESULTS/resultlist" "$RESULTS/results_aln.m8" "$RESULTS/tmp" --comp-bias-corr 1 -e inf --mask 1 --k-score 0 --max-kmer-per-pos 20 -k 6 --spaced-kmer-mode 0 --exact-kmer-matching 0 --seed-sub-mat PAM30.out --req-kmer-matches 1
